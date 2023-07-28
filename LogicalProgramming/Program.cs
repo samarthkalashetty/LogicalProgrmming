1837f7c5be6fd3af0ae222c5619cbf8177e01992
samarthkalashetty <samarthkalashetti2612@gmail.com>
2023-07-28 15:30:24 +0530

    [welcome to LOgicalProgramming]

---
+++ LogicalProgramming/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LogicalProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Logical Programming");
+            Console.ReadLine();        }
+    }
+}
